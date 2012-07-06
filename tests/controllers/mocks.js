4e4397dce31acc45143ae25eb078ea423bf0d25a
Jordan Santell <jsantell@gmail.com>
2012-07-05 22:54:10 -0400

    Separate routing controls

--- tests/controllers/mocks.js
+++
@@ -1,7 +0,0 @@
-module.exports = function ( app, model, args ) {
-  args.test.ok( app.name === 'thisIsExpress', 'express app has been passed into route for routing');
-  args.test.ok( model.name === 'mockSchema', 'Corresponding model has been passed into route');
-  args.test.ok( model.init, 'Model has been initialized via setupFunction in initModels when it\'s passed into the route' );
-  args.test.ok( args.name === 'controllerArgs', 'controller arguments succesfully passed into route');
-  args.test.done();
-};
