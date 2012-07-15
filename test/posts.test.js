f17dd596e369057b8a5cb73462c3dacacf6c1f3f
Jordan Santell <jsantell@gmail.com>
2012-07-15 15:47:51 -0400

    Posts should be ordered in reverse chrono, not chrono

--- test/posts.test.js
+++ test/posts.test.js
@@ -18,21 +18,21 @@ describe( 'Posts', function () {
       swag.set({ posts: './test/_postsJson', metaFormat: 'json' }).init(function () {
         var posts = app._locals.allPosts;
         posts.should.have.length(3);
-        posts[0].title.should.equal('Test Post One');
-        posts[0].tags.should.have.length(2);
-        posts[0].tags.should.include('a');
-        posts[0].tags.should.include('b');
-        posts[0].category.should.equal('testing');
-        posts[0].url.should.equal('/post/test1');
-        posts[0].arbitrary.should.equal('arbitrary content');
+        posts[2].title.should.equal('Test Post One');
+        posts[2].tags.should.have.length(2);
+        posts[2].tags.should.include('a');
+        posts[2].tags.should.include('b');
+        posts[2].category.should.equal('testing');
+        posts[2].url.should.equal('/post/test1');
+        posts[2].arbitrary.should.equal('arbitrary content');
     
         // Also tests HTML rendering
-        posts[0].preview.should.equal( postPreview );
-        posts[0].content.should.equal( postBody );
+        posts[2].preview.should.equal( postPreview );
+        posts[2].content.should.equal( postBody );
       
         // All posts should be in order
         posts[1].title.should.equal('Test Post Three');
-        posts[2].title.should.equal('Test Post Two');
+        posts[0].title.should.equal('Test Post Two');
         done();
       });
     });
@@ -47,21 +47,21 @@ describe( 'Posts', function () {
       swag.set({ posts: './test/_postsYaml', metaFormat: 'yaml' }).init(function () {
         var posts = app._locals.allPosts;
         posts.should.have.length(3);
-        posts[0].title.should.equal('Test Post One');
-        posts[0].tags.should.have.length(2);
-        posts[0].tags.should.include('a');
-        posts[0].tags.should.include('b');
-        posts[0].category.should.equal('testing');
-        posts[0].url.should.equal('/post/test1');
-        posts[0].arbitrary.should.equal('arbitrary content');
+        posts[2].title.should.equal('Test Post One');
+        posts[2].tags.should.have.length(2);
+        posts[2].tags.should.include('a');
+        posts[2].tags.should.include('b');
+        posts[2].category.should.equal('testing');
+        posts[2].url.should.equal('/post/test1');
+        posts[2].arbitrary.should.equal('arbitrary content');
     
         // Also tests HTML rendering
-        posts[0].preview.should.equal( postPreview );
-        posts[0].content.should.equal( postBody );
+        posts[2].preview.should.equal( postPreview );
+        posts[2].content.should.equal( postBody );
       
         // All posts should be in order
         posts[1].title.should.equal('Test Post Three');
-        posts[2].title.should.equal('Test Post Two');
+        posts[0].title.should.equal('Test Post Two');
         done();
       });
     });
