4299514ab12b034f5fb0091d719daba45c45ecb0
Jordan Santell <jsantell@gmail.com>
2012-07-16 08:43:41 -0400

    Start of routing tests

---
+++ test/routes.test.js
@@ -0,0 +1,35 @@
+var
+  express = require( 'express' ),
+  chai    = require( 'chai' ),
+  should  = chai.should(),
+  expect  = chai.expect;
+
+describe( 'Routes', function () {
+  it( 'should pass in the locals for the view', function ( done ) {
+    var
+      app = express.createServer(),
+      swag = require( '../lib/swag-blog' )( app );
+
+    swag.set({ posts: './test/_postsJson', metaFormat: 'json' })
+      .createPostRoute()
+      .createPostListRoute()
+      .createTagRoute()
+      .createCategoryRoute()
+      .init(function () {
+      app._locals.postCount.should.equal(3);
+      app._locals.allPosts.should.have.length(3);
+      app._locals.allTags.should.include('a');
+      app._locals.allTags.should.include('b');
+      app._locals.allTags.should.include('c');
+      app._locals.allTags.should.include('d');
+      app._locals.allCategories.should.include('testing');
+      app._locals.allCategories.should.include('other cat');
+      app._locals.pageUrl(3).should.equal('/posts/3');
+      app._locals.tagUrl('sometag').should.equal('/tag/sometag');
+      app._locals.categoryUrl('somecat').should.equal('/category/somecat');
+      app._locals.getPosts(2,3).should.have.length(1);
+      app._locals.pageCount.should.equal(1);
+      done();
+    });
+  });
+});
