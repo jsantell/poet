39f490e6cbe5dc0cc500836ce7ca56d456503ecc
Jordan Santell <jsantell@gmail.com>
2013-06-30 23:42:42 -0700

    Fix up last of tests

--- test/posts.test.js
+++ test/posts.test.js
@@ -1,8 +1,9 @@
 var
-  express = require( 'express' ),
-  chai    = require( 'chai' ),
-  should  = chai.should(),
-  expect  = chai.expect;
+  Poet = require('../lib/poet'),
+  express = require('express'),
+  chai = require('chai'),
+  should = chai.should(),
+  expect = chai.expect;
 
 var
   postPreview = '<p><em>Lorem ipsum</em> dolor sit amet, consectetur adipisicing elit.</p>',
@@ -11,31 +12,30 @@ var
   readMoreAnchorp2 = '<p><a href="/post/test2" title="Read more of Test Post Two">read more</a></p>';
   readMoreAnchorp3 = '<p><a href="/post/test3" title="Read more of Test Post Three">read more</a></p>';
 
-describe( 'Posts', function () {
-  describe( 'Posts with JSON front-matter', function () {
-    it( "should store all the post's metadata correctly", function ( done ) {
+describe('Posts', function () {
+  describe('Posts with JSON front-matter', function () {
+    it('should store all the post\'s metadata correctly', function (done) {
       var
         app = express(),
-        poet = require( '../lib/poet' )( app );
+        poet = Poet(app, {
+          posts: './test/_postsJson',
+          showDrafts: true
+        });
 
-      // Should default to json
-      poet.set({ 
-        posts: './test/_postsJson',
-        showDrafts: true
-      }).init(function () {
-        var posts = app.locals.postList;
+      poet.init().then(function () {
+        var posts = poet.helpers.getPosts();
         posts.should.have.length(6);
-        posts[2].slug.should.equal('test1');
-        posts[2].tags.should.have.length(2);
-        posts[2].tags.should.include('a');
-        posts[2].tags.should.include('b');
-        posts[2].category.should.equal('testing');
-        posts[2].url.should.equal('/post/test1');
-        posts[2].arbitrary.should.equal('arbitrary content');
+        poet.posts['test1'].slug.should.equal('test1');
+        poet.posts['test1'].tags.should.have.length(2);
+        poet.posts['test1'].tags.should.include('a');
+        poet.posts['test1'].tags.should.include('b');
+        poet.posts['test1'].category.should.equal('testing');
+        poet.posts['test1'].url.should.equal('/post/test1');
+        poet.posts['test1'].arbitrary.should.equal('arbitrary content');
 
         // Also tests HTML rendering
-        posts[2].preview.should.equal( postPreview + "\n" + readMoreAnchorp1 );
-        posts[2].content.should.equal( postBody );
+        poet.posts['test1'].preview.should.equal(postPreview + "\n" + readMoreAnchorp1 );
+        poet.posts['test1'].content.should.equal(postBody);
 
         // All posts should be in order
         posts[5].title.should.equals('Test Post Four - A Draft');
@@ -44,42 +44,44 @@ describe( 'Posts', function () {
         posts[2].title.should.equal('Test Post One');
         posts[1].title.should.equal('Test Post Three');
         posts[0].title.should.equal('Test Post Two');
-        
+
         // One test post is a draft
         posts[5].draft.should.equal(true);
 
         // Preview parameter should work and turn into html
-        posts[0].preview.should.equal( '<p><em>some content</em></p>' + "\n" + readMoreAnchorp2 );
-
+        posts[0].preview.should.equal('<p><em>some content</em></p>' + '\n' + readMoreAnchorp2 );
         done();
-      });
+      }).then(null, done);
     });
 
-    it( 'should format preview according to previewLength', function ( done ) {
+    it('should format preview according to previewLength', function (done) {
       var
         app = express(),
-        poet = require( '../lib/poet' )( app );
+        poet = Poet(app, {
+          posts: './test/_postsJson',
+          showDrafts: true
+        });
 
-      poet.set({ posts: './test/_postsJson', metaFormat: 'json' }).init(function () {
-        var posts = app.locals.postList;
+      poet.init().then(function () {
+        var posts = poet.helpers.getPosts();
         posts[1].preview.should.equal('<p><em>Lorem ipsum</em> dolor sit amet</p>' + "\n" + readMoreAnchorp3);
         done();
       });
     });
   });
 
-  describe( 'Posts with YAML front-matter', function () {
-    it( "should store all the post's metadata correctly", function ( done ) {
+  describe('Posts with YAML front-matter', function () {
+    it("should store all the post's metadata correctly", function (done) {
       var
         app = express(),
-        poet = require( '../lib/poet' )( app );
+        poet = Poet(app, {
+          posts: './test/_postsYaml',
+          showDrafts: true,
+          metaFormat: 'yaml'
+        });
 
-      poet.set({ 
-        posts: './test/_postsYaml',
-        metaFormat: 'yaml',
-        showDrafts: true
-      }).init(function () {
-        var posts = app.locals.postList;
+      poet.init().then(function () {
+        var posts = poet.helpers.getPosts();
         posts.should.have.length(4);
         posts[2].title.should.equal('Test Post One');
         posts[2].slug.should.equal('test1');
@@ -103,20 +105,23 @@ describe( 'Posts', function () {
         posts[0].preview.should.equal( '<p><em>some content</em></p>' + "\n" + readMoreAnchorp2);
 
         done();
-      });
+      }).then(null, done);
     });
 
-    it( 'should format preview according to previewLength', function ( done ) {
+    it('should format preview according to previewLength', function (done) {
       var
         app = express(),
-        poet = require( '../lib/poet' )( app );
+        poet = Poet(app, {
+          posts: './test/_postsYaml',
+          showDrafts: true,
+          metaFormat: 'yaml'
+        });
 
-      poet.set({ posts: './test/_postsYaml', metaFormat: 'yaml' }).init(function () {
-        var posts = app.locals.postList;
+      poet.init().then(function () {
+        var posts = poet.helpers.getPosts();
         posts[1].preview.should.equal('<p><em>Lorem ipsum</em> dolor sit amet</p>' + "\n" + readMoreAnchorp3);
         done();
-      });
+      }).then(null, done);
     });
   });
-
 });
