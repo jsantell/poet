c7599acb22b7505428f6771375e523faa13249a8
Jordan Santell <jsantell@gmail.com>
2013-06-30 22:17:13 -0700

    Working tests

--- test/locals.test.js
+++ test/locals.test.js
@@ -1,33 +1,32 @@
 var
-  express = require( 'express' ),
-  chai    = require( 'chai' ),
-  should  = chai.should(),
-  expect  = chai.expect;
+  Poet = require('../lib/poet'),
+  express = require('express'),
+  chai = require('chai'),
+  expect = chai.expect;
 
-describe( 'Locals', function () {
-  it( 'should pass in the locals for the view', function ( done ) {
+describe('Locals', function () {
+  it('should pass in the locals for the view', function (done) {
     var
       app = express(),
-      poet = require( '../lib/poet' )( app );
+      poet = Poet(app, {
+        posts: './test/_postsYaml',
+        metaFormat: 'yaml'
+      });
 
-    poet.set({ 
-      posts: './test/_postsYaml',
-      metaFormat: 'yaml',
-      showDrafts: true
-    }).init(function () {
-      app.locals.getPostCount().should.equal(4);
-      app.locals.postList.should.have.length(4);
-      app.locals.tagList.should.include('a');
-      app.locals.tagList.should.include('b');
-      app.locals.tagList.should.include('c');
-      app.locals.tagList.should.include('d');
-      app.locals.categoryList.should.include('testing');
-      app.locals.categoryList.should.include('other cat');
-      app.locals.pageUrl(3).should.equal('/page/3');
-      app.locals.tagUrl('sometag').should.equal('/tag/sometag');
-      app.locals.categoryUrl('somecat').should.equal('/category/somecat');
-      app.locals.getPosts(2,3).should.have.length(1);
-      app.locals.getPageCount().should.equal(1);
+    poet.init().then(function () {
+      expect(app.locals.getPostCount()).to.equal(4);
+      expect(app.locals.getPosts()).to.have.length(4);
+      expect(app.locals.getTags()).to.include('a');
+      expect(app.locals.getTags()).to.include('b');
+      expect(app.locals.getTags()).to.include('c');
+      expect(app.locals.getTags()).to.include('d');
+      expect(app.locals.getCategories()).to.include('testing');
+      expect(app.locals.getCategories()).to.include('other cat');
+      expect(app.locals.pageUrl(3)).to.equal('/page/3');
+      expect(app.locals.tagUrl('sometag')).to.equal('/tag/sometag');
+      expect(app.locals.categoryUrl('somecat')).to.equal('/category/somecat');
+      expect(app.locals.getPosts(2,3)).to.have.length(1);
+      expect(app.locals.getPageCount()).to.equal(1);
       done();
     });
   });
