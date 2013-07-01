ed0a1155695c1f8ad299b4b3a242ad6092f8b219
Jordan Santell <jsantell@gmail.com>
2013-06-30 20:53:00 -0700

    Add watcher support

---
+++ examples/watcher.js
@@ -0,0 +1,22 @@
+var
+  express = require('express'),
+  app = express(),
+  // All default options
+  poet = require('../lib/poet')(app);
+
+poet.init().then(function () {
+  // initialized
+});
+
+poet.watch(function () {
+  // watcher reloaded
+});
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+app.get('/', function (req, res) { res.render('index'); });
+
+app.listen(3000);
