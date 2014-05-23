d0905f36bfd7631a8ae95f755a4dbf3e6fe2a7cb
Adam Beck <AdamB.Infinity@gmail.com>
2014-05-22 23:14:11 -0400

    Added support Express 4.0.0

--- test/helpers/routeInfo.js
+++ test/helpers/routeInfo.js
@@ -1,18 +1,10 @@
 exports.getCallback = function ( app, path ) {
 
-  if (app.routes && app.routes.get) {
-    for (var i = 0; i < app.routes.get.length; i++) {
-      if (app.routes.get[i].path == path) {
-        return app.routes.get[i].callbacks[0];
-      }
-    }
-  } else {
-    if (app._router && app._router.stack) {
-      for (var i = 0; i < app._router.stack.length; i++) {
-        var current = app._router.stack[i];
-        if (current.route && current.route.path && current.route.path == path) {
-          return current.route.stack[0].handle;
-        }
+  if (app._router && app._router.stack) {
+    for (var i = 0; i < app._router.stack.length; i++) {
+      var current = app._router.stack[i];
+      if (current.route && current.route.path && current.route.path == path) {
+        return current.route.stack[0].handle;
       }
     }
   }
